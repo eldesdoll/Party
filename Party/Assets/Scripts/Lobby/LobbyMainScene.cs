8f006ba98a0391c39243177837c80a4e4f2c6204
eldesdoll <38787939+eldesdoll@users.noreply.github.com>
2019-06-27 22:15:03 -0500

    Menu de inicio 2

---
+++ Party/Assets/Scripts/Lobby/LobbyMainScene.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LobbyMainScene : MonoBehaviour
+{
+    [Header("Login Panel")]
+    public GameObject loginPanel;
+
+    public TMP_InputField playerNameInput;
+
+    [Header("Selection Panel")]
+    public GameObject selectionPanel;
+
+    [Header("Create Room Panel")]
+    public GameObject createRoomPanel;
+
+    public TMP_InputField roomNameInputField;
+
+    [Header("Join Random Room Panel")]
+    public GameObject joinRandomRoomPanel;
+
+    [Header("Room List Panel")]
+    public GameObject roomListPanel;
+
+    public GameObject roomListContent;
+    public GameObject roomListEntryPrefab;
+
+    [Header("Inside Room Panel")]
+    public GameObject insideRoomPanel;
+
+    public Button startGameButton;
+    public GameObject playerListEntryPrefab;
+
+    private int MaxPlayers = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Party/Assets/Scripts/Lobby/PlayerListEntry.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+
+public class PlayerListEntry : MonoBehaviour
+{
+    [Header("UI References")]
+    public TextMeshPro PlayerNameText;
+
+    public Image PlayerColorImage;
+    public Button PlayerReadyButton;
+    public Image PlayerReadyImage;
+
+    private int ownerId;
+    private bool isPlayerReady;
+
+}
